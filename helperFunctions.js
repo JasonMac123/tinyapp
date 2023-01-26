a0f903769f01163185788bab1be36c7487037408
JasonSnow123 <m.jason.2018@gmail.com>
2023-01-26 10:27:32 -0500

    refactored code to use dataset module

--- helperFunctions.js
+++ helperFunctions.js
@@ -1,3 +1,5 @@
+const { urlDatabase, users} = require('./dataset');
+
 //generates a 6 digit random alphanumeric string
 const generateRandomString = () => {
   let randomString = "";
@@ -9,19 +11,19 @@ const generateRandomString = () => {
   return randomString;
 };
 //checks if the email string value is within the user database for registered users
-const checkEmail = (database, newEmail) => {
-  for (const user in database) {
-    if (database[user].email === newEmail)
+const checkEmail = (newEmail) => {
+  for (const user in users) {
+    if (users[user].email === newEmail)
       return true; //boolean result if there an email within the database
   }
   return false;
 };
 
 //checks if the password matches the email given in the database
-const checkPassword = (database, newEmail, pass) => {
-  for (const user in database) {
-    if (database[user].email === newEmail) { //locates if the email matches
-      if (database[user].password === pass) { //checks if the password matches
+const checkPassword = (newEmail, pass) => {
+  for (const user in users) {
+    if (users[user].email === newEmail) { //locates if the email matches
+      if (users[user].password === pass) { //checks if the password matches
         return user; //returns the user id to be used
       }
       break; //breaks out of the loop as there are no duplicate emails within the database
@@ -29,11 +31,12 @@ const checkPassword = (database, newEmail, pass) => {
   }
   return false;
 };
-
-const urlsForUser = function(id,database) {
+//generates an array of allowed link ids that match the user_id in the object database
+const urlsForUser = function(id) {
   const urlArray = [];
-  for (const urlID in database) {
-    if ((database[urlID].userID) === id) {
+
+  for (const urlID in urlDatabase) {
+    if ((urlDatabase[urlID].userID) === id) { //checking the userID, if it matches the cookie id
       urlArray.push(urlID);
     }
   }
